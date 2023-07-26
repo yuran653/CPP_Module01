f958f9bb4b17a1f9e92b74658ee756c635aedfba
Iurii Ovsiannikov <jgoldste@z3t10c2.42bangkok.com>
2023-07-27 03:49:51 +0700

    ex01_is_done

--- ex01/include/Zombie.hpp
+++ ex01/include/Zombie.hpp
@@ -6,12 +6,14 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 02:12:19 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/07/27 02:59:54 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/07/27 03:43:00 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
+
+#define SIZE 5
 
 #include <iostream>
 #include <string>
@@ -25,8 +27,8 @@ class Zombie {
 		Zombie(std::string name);
 		~Zombie();
 
-		void		announce(void);
-		void		setName(std::string name);
+		void	announce(void);
+		void	setName(std::string name);
 };
 
 Zombie*	zombieHorde(int N, std::string name);
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,15 +6,23 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 02:57:38 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/07/27 03:07:30 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/07/27 03:46:05 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main() {
-	Zombie* zombie_horde = zombieHorde(5, "Zombie");
-	delete
+	Zombie* zombie_horde;
+	zombie_horde = zombieHorde(SIZE, "Zombie");
+	for (Zombie* ptr = zombie_horde; ptr != zombie_horde + SIZE; ptr++) {
+		ptr->setName("RenamedZombie");
+	}
+	for (int i = SIZE - 1; i >= 0; i--) {
+		std::cout << "[" << i << "]";
+		zombie_horde[i].announce();
+	}
+	delete[] zombie_horde;
 
 	return 0;
 }
